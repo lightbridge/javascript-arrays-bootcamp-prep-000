b838b4503092ea25bd88e2cacddf959cae230640
Chad Bishop <connect@ltbrdg.com>
2018-02-27 02:28:53 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars =
+[
+  'snickers', 
+  'hundred grand', 
+  'kitkat', 
+  'skittles'
+  ]
+  
+ function addElementToBeginningOfArray(){
+   chocolateBars.unshift("whutchamacallit");
+ }
+  
+   function destructivelyAddElementToBeginningOfArray(){
+   chocolateBars.push("whutchamacallit");
+ }
